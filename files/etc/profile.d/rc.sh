d1ec1e5740c39fc14041b8e8de935f715cee53ca
Yann Autissier <yann.autissier@gmail.com>
2016-12-20 17:27:41 +0100

    Improve detection of interactive shells.
    Ansible uses interactive shells to manage remote servers.
    Loading user stuffs can disturb ansible and the test on STDIN is not
    good enough when ansible connects with an interactive shell.

--- files/etc/profile.d/rc.sh
+++ files/etc/profile.d/rc.sh
@@ -1,17 +1,22 @@
 # verify that default functions are loaded
 type force >/dev/null 2>&1 || source /etc/profile.d/functions.sh 2>/dev/null
 
-# if we are in a terminal and automatic stuffs are enabled
-if [ -t 0 -a -d ${HOME}/.rc.d ]; then
+# test current shell flags
+case $- in
+  # if we are in an interactive shell
+  *i*)
+    # load user defined stuffs
     for user_func in ${HOME}/.rc.d/*; do
-        # search for files
+        # read files only
         [ -f ${user_func} ] && func_name="$(basename ${user_func})" || continue
-        # func_name should start with a letter (allow to order function calls with names starting with numbers)
+        # at this stage, func_name can start with numbers to allow ordering function calls with file names starting with numbers
+        # func_name must start with a letter, remove all other characters at the beginning of func_name until a letter is found
         while [ "${func_name}" != "" -a "${func_name#[a-z]}" = "${func_name}" ]; do
             # remove first char of func_name
             func_name="${func_name#?}"
         done
-        # call user function with args passed from the content of the file
+        # call user function with args passed from the content of the user_func file
         [ -n "${func_name}" ] && ${func_name} $(cat ${user_func}) 2>/dev/null
     done
-fi
+  ;;
+esac
